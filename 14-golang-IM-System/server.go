90d04abd38ace8211f8879e7e72a45a144e7a7a7
alatai <alatai0013@gmail.com>
2023-07-05 15:19:55 +0900

    finished IM-System demo

--- 14-golang-IM-System/server.go
+++ 14-golang-IM-System/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -40,7 +42,7 @@ func (server *Server) Start() {
 	}
 
 	// 启动监听Message的goroutine
-	go server.listenMessage()
+	go server.ListenMessage()
 
 	for {
 		// accept
@@ -62,28 +64,72 @@ func (server *Server) Start() {
 
 // handler具体实现
 func (server *Server) handler(conn net.Conn) {
-	user := NewUser(conn)
+	user := NewUser(conn, server)
+	// 用户上线
+	user.Online()
 
-	// 用户上线，将用户加入到onlineMap中
-	server.mapLock.Lock()
-	server.OnlineMap[user.Name] = user
-	server.mapLock.Unlock()
+	// 监听用户是否活跃的channel
+	isLive := make(chan bool)
 
-	// 广播当前用户上线消息
-	server.broadCase(user, "已上线")
+	// 接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			// read：读取的长度
+			read, err := conn.Read(buf)
+			if read == 0 {
+				// 用户下线
+				user.Offline()
+				return
+			}
+
+			// EOF：文件末尾
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err:", err)
+				return
+			}
+
+			// 提取用户的消息（去除'\n'）
+			msg := string(buf[:read-1])
+			// 用于针对msg进行处理
+			user.DoMessage(msg)
+			// 用户的任意消息，代表当前用户时活跃状态
+			isLive <- true
+		}
+	}()
 
 	// 当前handler阻塞
-	select {}
+	for {
+		// select会阻塞当前channel
+		select {
+		// 当前用户是活跃的
+		case <-isLive:
+		// 不在任何处理，激活下一个case的语句，达到重置定时器的目的
+		// time.After：golang中的定时器
+		// time.After()执行后重置该定时器
+		case <-time.After(time.Second * 120):
+			// 已经超时，将当前的用户强制关闭
+			user.SendMsg("您被下线\n")
+			// 销毁用户资源
+			close(user.Channel)
+			// 关闭连接
+			//goland:noinspection GoUnhandledErrorResult
+			conn.Close()
+			// 退出当前handler
+			// runtime.exit()
+			return
+		}
+	}
 }
 
-// 广播消息
-func (server *Server) broadCase(user *User, msg string) {
+// BroadCase 广播消息
+func (server *Server) BroadCase(user *User, msg string) {
 	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
 	server.Message <- sendMsg
 }
 
-// 监听Message广播消息channel的goroutine，一旦有消息就发送给全部的在线用户
-func (server *Server) listenMessage() {
+// ListenMessage 监听Message广播消息channel的goroutine，一旦有消息就发送给全部的在线用户
+func (server *Server) ListenMessage() {
 	for {
 		msg := <-server.Message
 		// 将msg发送给全部的在线用户
--- 14-golang-IM-System/user.go
+++ 14-golang-IM-System/user.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
 	Name string
@@ -9,10 +12,12 @@ type User struct {
 	Channel chan string
 	// 当前用户与客户端通信的连接
 	Conn net.Conn
+	// 当前用户属于哪个Server
+	Server *Server
 }
 
 // NewUser 创建一个用户
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 
 	user := &User{
@@ -20,6 +25,7 @@ func NewUser(conn net.Conn) *User {
 		Addr:    userAddr,
 		Channel: make(chan string),
 		Conn:    conn,
+		Server:  server,
 	}
 
 	// 启动监听当前用户channel消息的goroutine
@@ -34,4 +40,91 @@ func (user *User) listenMessage() {
 		//goland:noinspection GoUnhandledErrorResult
 		user.Conn.Write([]byte(msg + "\n"))
 	}
+}
+
+// Online 用户上线功能
+func (user *User) Online() {
+	// 用户上线，将用户加入到onlineMap中
+	user.Server.mapLock.Lock()
+	user.Server.OnlineMap[user.Name] = user
+	user.Server.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	user.Server.BroadCase(user, "已上线")
+}
+
+// Offline 用户下线功能
+func (user *User) Offline() {
+	// 用户下线，将用户加入到onlineMap中
+	user.Server.mapLock.Lock()
+	delete(user.Server.OnlineMap, user.Name)
+	user.Server.mapLock.Unlock()
+
+	// 广播当前用户下线消息
+	user.Server.BroadCase(user, "已下线")
+}
+
+// DoMessage 用户处理消息功能
+func (user *User) DoMessage(msg string) {
+	// 通过“who”指令查询在线用户
+	if msg == "who" {
+		user.Server.mapLock.Lock()
+		for _, onlineUser := range user.Server.OnlineMap {
+			onlineMsg := "[" + onlineUser.Addr + "]" + onlineUser.Name + " : 在线...\n"
+			// 将该消息发送给当前用户
+			user.SendMsg(onlineMsg)
+		}
+		user.Server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" { // 通过“rename”指令修改用户名
+		// 消息格式：rename|newName
+		// Split：通过指定字符截取字符，截取的内容放到数组中
+		newName := strings.Split(msg, "|")[1] // [rename, newName]
+		// 判断newName是否已经存在
+		_, ok := user.Server.OnlineMap[newName]
+		if ok {
+			user.SendMsg("当前用户名称已经被使用\n")
+		} else {
+			user.Server.mapLock.Lock()
+			// 删除旧名称的用户（服务器登录用户）
+			delete(user.Server.OnlineMap, user.Name)
+			// 创建新名称用户（服务器登录用户）
+			user.Server.OnlineMap[newName] = user
+			user.Server.mapLock.Unlock()
+			// 更新用户名
+			user.Name = newName
+			user.SendMsg("您已经更新用户名：" + user.Name + "\n")
+		}
+	} else if len(msg) > 4 && msg[:3] == "to|" { // 通过“to”指令完成私聊
+		// 消息格式：to|$username$|msg”
+		// 获取对方用户名
+		remoteName := strings.Split(msg, "|")[1] // [to, remoteName, msg]
+		if remoteName == "" {
+			user.SendMsg("消息格式不正确，请使用\"to|name|msg\"格式\n")
+			return
+		}
+
+		// 根据用户名得到User对象
+		remoteUser, ok := user.Server.OnlineMap[remoteName]
+		// 当前用户不存在
+		if !ok {
+			user.SendMsg("该用户名不存在\n")
+			return
+		}
+
+		// 获取消息内容，发送给指定用户
+		content := strings.Split(msg, "|")[2] // [to, remoteName, msg]
+		if content == "" {
+			user.SendMsg("无消息内容，请重新发送\n")
+			return
+		}
+		remoteUser.SendMsg(user.Name + "对您说：" + content + "\n")
+	} else {
+		user.Server.BroadCase(user, msg)
+	}
+}
+
+// SendMsg 给指定用户发送消息
+func (user *User) SendMsg(msg string) {
+	//goland:noinspection GoUnhandledErrorResult
+	user.Conn.Write([]byte(msg))
 }
